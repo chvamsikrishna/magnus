79af8e9fbc834a96db20eb2c511ae21a724f9fca
vAmsi <vamsi@gmail.com>
2018-08-09 02:08:50 -0500

    Commiting Git class to Git hub.

---
+++ Magnus/src/Git/Basic.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class Basic {
+	public void toDo()
+	{
+		System.out.println("Hello git-hub");
+	}
+
+	public static void main(String[] args) {
+		
+		Basic b = new Basic();
+		b.toDo();
+    
+		 
+	}
+
+}
